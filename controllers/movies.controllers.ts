47e66468e67ab9769e0deb3fddc74c476ecd0267
Teepee78 <eniolatoluwalase78@gmail.com>
2023-05-29 02:49:42 +0100

    Create endpoint to book seats to a movie

--- controllers/movies.controllers.ts
+++ controllers/movies.controllers.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import Movie from "../models/movies.models";
+import Ticket from "../models/tickets.models";
+import User from "../models/users.models";
+import { sem } from "..";
+import { I_TicketDocument } from "../models";
 
 /**
  * Create a new movie in the database
@@ -42,7 +46,67 @@ export async function getMovieById(req: Request, res: Response) {
 
 		const movie = await Movie.findById(id);
 
-		return res.json(movie)
+		return res.json(movie);
+	} catch (err: any) {
+		console.log(err);
+		return res.status(500).json({ message: err.message });
+	}
+}
+
+/**
+ * Book seats to a movie
+ */
+export async function bookMovieSeats(req: Request, res: Response) {
+	try {
+		const { movieId } = req.query;
+		const seats = Number(req.query.seats);
+		const userId = req.body.id;
+
+		// Get movie
+		const movie = await Movie.findById(movieId);
+		if (!movie) return res.status(404).json({ message: "Movie not found" });
+
+		// Get user
+		const user = await User.findById(userId);
+
+		// Make sure there are enough available tickets for requested seats
+		// and movie isn't sold out
+		let availableTickets = movie.availableTickets;
+		if (availableTickets <= 0)
+			return res.status(406).json({ message: "Movie sold out" });
+		if (seats > availableTickets && availableTickets > 0)
+			return res.status(406).json({
+				message: "Not enough seats available to meet requested seats",
+			});
+
+		sem.take(async () => {
+			let tickets: Array<I_TicketDocument> = [];
+			// create ticket for each seat
+			for (let i = 0; i < seats; i++) {
+				let ticket = new Ticket({
+					number: movie.capacity - availableTickets + 1,
+					movieId: movie?.id,
+					userId: userId,
+				});
+
+				// update references to ticket
+				await ticket.save();
+				tickets.push(ticket);
+				movie.availableTickets -= 1;
+				movie.tickets.push(ticket._id);
+				await movie?.save();
+				user!.tickets.push(ticket._id);
+				await user!.save();
+				availableTickets = movie.availableTickets;
+			}
+
+			sem.leave();
+
+			return res.status(201).json({
+				message: `${seats} Ticket(s) for ${movie.title} created successfully`,
+				tickets: tickets,
+			});
+		});
 	} catch (err: any) {
 		console.log(err);
 		return res.status(500).json({ message: err.message });
--- routes/movies.routes.ts
+++ routes/movies.routes.ts
@@ -2,6 +2,7 @@ import {
 	createMovie,
 	getMovies,
 	getMovieById,
+	bookMovieSeats,
 } from "../controllers/movies.controllers";
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middlewares";
@@ -9,7 +10,8 @@ import { auth } from "../middlewares/auth.middlewares";
 const router = Router();
 
 router.post("", createMovie);
-router.get("", auth, getMovies);
+router.get("", getMovies);
 router.get("/:id", getMovieById);
+router.post("/book", auth, bookMovieSeats);
 
 export default router;
